eb958ed1248de3cdc99f4e8826dc48abd79ac715
wilfredlams <wilfrednetwork@gmail.com>
2019-05-10 17:04:57 +1000

    exception check

--- VPN Install Application/runVPNInstallers.cs
+++ VPN Install Application/runVPNInstallers.cs
@@ -41,16 +41,15 @@ namespace VPN_Install_Application
                     list = stateArray[4].Split(',').ToList();
                     vpnsubdirs = stateArray[5].Split(',');
                 }
-                File.Delete(statefile);
+                //File.Delete(statefile);
 
                 try
                 {
                     var rWrite = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce", true);
                     rWrite.DeleteValue("mtivpninstaller");
+                    File.Delete(statefile);
                 }
-                catch { }
-
-                File.Delete(statefile);
+                catch (Exception ex){ MessageBox.Show(ex.Message); }
 
                 change_appstate();
                 nextCounter();
