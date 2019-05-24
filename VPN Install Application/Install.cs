71b6d6c24795b7690bfb7019ca0e8e55c7764d9e
MegaHobbit <brendanmatthys@outlook.com>
2019-05-24 11:57:54 +1000

    Added more supported file extension types

--- VPN Install Application/Install.cs
+++ VPN Install Application/Install.cs
@@ -138,9 +138,11 @@ namespace VPN_Install_Application
 
                     string Containing = fi.FullName;
                     if (Containing.Contains(".txt") || Containing.Contains(".rdp") || Containing.Contains(".ps1") ||
-                        Containing.Contains(".bat") || Containing.Contains(".lnk") ||
+                        Containing.Contains(".rcf") ||
+                        Containing.Contains(".bat") || Containing.Contains(".lnk") || Containing.Contains(".pfx") ||
                         Containing.Contains(".TXT") || Containing.Contains(".RDP") || Containing.Contains(".PS1") ||
-                        Containing.Contains(".BAT") || Containing.Contains(".LNK"))
+                        Containing.Contains(".BAT") || Containing.Contains(".LNK") || Containing.Contains(".PFX") ||
+                        Containing.Contains(".RCF"))
                     {
                         Debug.WriteLine("Copying file: " + filetarget.FullName + fi.Name);
                         fi.CopyTo(Path.Combine(filetarget.FullName, fi.Name), true);
